bcdf5dab66c607846c70c01e6b7cd76ba2df7eba
Jessica Francavilla <jessica.francavilla@aerolab.co>
2018-05-08 20:35:21 -0300

    add edit button

--- src/App.js
+++ src/App.js
@@ -1,31 +1,17 @@
 import React, {Component} from 'react';
 import Header from './Header/Header';
-import Grid from './Grid/Grid';
-import Sidebar from './Sidebar/Sidebar';
 
 import './App.css';
 
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
-
-
 class App extends Component {
-    titleInput;
-    descriptionInput;
 
     render() {
-        return (
 
-            <Router>
-                <div className="App">
+        return (
+            <div className="App">
                 <Header/>
-                <div className="containerApp">
-                    <Sidebar/>
-                    <Grid/>
-                      
-                    </div>
-                </div>
-            </Router>
-        );    
+            </div>
+        );
     }
 }
 
